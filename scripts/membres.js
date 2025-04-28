29bc1a4238273a1877926bd6c7f7f834b0cff0ef
Artic <artic.admisoffi@gmail.com>
2025-04-28 14:44:36 +0200

    feat: :sparkles: Ajout des Profiles

--- scripts/membres.js
+++ scripts/membres.js
@@ -3,14 +3,17 @@ const memberList = document.getElementById("membres-list");
 function addMember(member) {
     /*
     <article class="membres-membre">
-        <h2>Nom — Grade</h2>
+        <h2><a href="/membre/Nom">Nom — Grade</a></h2>
         <p>Description</p>
     </article>
     */
     const article = document.createElement("article");
     article.classList.add("membres-membre");
+    const a = document.createElement("a");
+    a.textContent = `${member.name} — ${member.grade}`;
+    a.href = `/membre/${member.name}`;
     const h2 = document.createElement("h2");
-    h2.textContent = `${member.name} — ${member.grade}`;
+    h2.appendChild(a); 
     const p = document.createElement("p");
     p.textContent = member.description;
     article.appendChild(h2);
