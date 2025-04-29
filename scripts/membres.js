41db17ba5eeb36c8c42a0e781471f7a7e36fc9bd
Artic <artic.admisoffi@gmail.com>
2025-04-29 14:54:59 +0200

    feat: :rocket: Mise à jour majeure
    Ajout du gtag.js, optimisation du SEO et du chargement du site

--- scripts/membres.js
+++ scripts/membres.js
@@ -2,23 +2,39 @@ const memberList = document.getElementById("membres-list");
 
 function addMember(member) {
     /*
-    <article class="membres-membre">
-        <h2><a href="/membre/Nom">Nom — Grade</a></h2>
-        <p>Description</p>
+    <article class="membres-membre" itemscope itemtype="https://schema.org/Person">
+        <h2>
+            <a href="/membre/Nom" title="Voir le profile">
+                <span itemprop="callSign">Nom</span>
+                <span> — Grade</span>
+            </a>
+        </h2>
+        <p itemprop="description">Description</p>
     </article>
     */
     const article = document.createElement("article");
     article.classList.add("membres-membre");
+    article.setAttribute("itemscope", "");
+    article.setAttribute("itemtype", "https://schema.org/Person");
+    const name = document.createElement("span");
+    name.textContent = member.name;
+    name.setAttribute("itemprop", "callSign");
+    const grade = document.createElement("span");
+    grade.textContent = ` — ${member.grade}`;
     const a = document.createElement("a");
-    a.textContent = `${member.name} — ${member.grade}`;
+    a.appendChild(name);
+    a.appendChild(grade);
     a.href = `/membre/${member.name}`;
+    a.setAttribute("title", "Voir le profile");
     const h2 = document.createElement("h2");
     h2.appendChild(a); 
     const p = document.createElement("p");
     p.textContent = member.description;
+    p.setAttribute("itemprop", "description");
     article.appendChild(h2);
     article.appendChild(p);
     memberList.appendChild(article);
+    return;
 }
 
 fetch("/data/membres.json")
