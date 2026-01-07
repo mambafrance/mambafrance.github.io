9b0a67730ff2d32d064c5be20e0d82960a59ffb6
Artic <artic.admisoffi@gmail.com>
2026-01-07 21:03:22 +0100

    feat: :art: Ajout de style dans la page membres

--- scripts/membres.js
+++ scripts/membres.js
@@ -1,12 +1,21 @@
 const memberList = document.getElementById("membres-list");
 
+GRADE = {
+    "Commandant": "commandant",
+    "Député": "depute",
+    "Officier": "officier",
+    "Sous-Officier": "sous-officier",
+    "Créateur de contenu": "createurdecontenu",
+    "Membre": "membre"
+}
+
 function addMember(member) {
     /*
     <article class="membres-membre" itemscope itemtype="https://schema.org/Person">
         <h2>
             <a href="/membre/Nom" title="Voir le profile">
                 <span itemprop="callSign">Nom</span>
-                <span> — Grade</span>
+                <span> — <span>Grade</span></span>
             </a>
         </h2>
         <p itemprop="description">Description</p>
@@ -20,19 +29,25 @@ function addMember(member) {
     name.textContent = member.name;
     name.setAttribute("itemprop", "callSign");
     const grade = document.createElement("span");
-    grade.textContent = ` — ${member.grade}`;
+    grade.textContent = member.grade;
+    grade.className = `${GRADE[member.grade]} shadow`;
+    const grade2 = document.createElement("span");
+    grade2.textContent = " — ";
+    grade2.appendChild(grade);
     const a = document.createElement("a");
     a.appendChild(name);
-    a.appendChild(grade);
+    a.appendChild(grade2);
     a.href = `/membre/${member.name}`;
     a.setAttribute("title", "Voir le profile");
     const h2 = document.createElement("h2");
     h2.appendChild(a); 
-    const p = document.createElement("p");
-    p.textContent = member.description;
-    p.setAttribute("itemprop", "description");
     article.appendChild(h2);
-    article.appendChild(p);
+    if (member.description != "Description \u00e0 ajouter.") {
+        const p = document.createElement("p");
+        p.textContent = member.description;
+        p.setAttribute("itemprop", "description");
+        article.appendChild(p);
+    }
     memberList.appendChild(article);
     return;
 }
