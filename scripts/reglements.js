70a60ebca6382a6b6776e9b4c199d57a080390c5
Artic <artic.admisoffi@gmail.com>
2026-03-13 06:44:43 +0100

    feat: :art: Amendement du règlement
    Modification système, une surbrillance apparait lors de la sélection d'article / Modification système, les liens hypertextes créent une nouvelle page web / Ajout d'une règle sD.6 relatif aux salons vocaux / Ajout d'une règle sD.9-2 relatif aux autorisations sur l'autopromotion et la publicité / Modification du TL;DR. E.1 (SRE -> SQB) / Modification graphique E.7-3 / Modification de l'article E.13 relatif à la protection des données et confidentialité

--- scripts/reglements.js
+++ scripts/reglements.js
@@ -1,10 +1,39 @@
-function terms(reglement, term) {
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+let title = null;
+
+async function terms(reglement, term) {
     let details = document.getElementById(`${reglement}`);
     details.open = true;
-    document.getElementById(`${reglement}${term}`)
-    .scrollIntoView({
+    let rule = document.getElementById(`${reglement}${term}`);
+    title = rule.getElementsByTagName("h2")[0];
+    if (`${term}`.includes(".")) {
+        title = rule;
+    }
+    title.scrollIntoView({
         block: "center",
         inline: "center"
     });
     return;
 }
+
+setInterval(async () => {
+    if (!title) { return; };
+    if (!isElementInViewport(title)) { return; };
+    if (!title) { return; };
+    title.classList.add("mark");
+    await sleep(2000);
+    if (!title) { return; };
+    title.classList.remove("mark");
+    title = null;
+}, 100);
